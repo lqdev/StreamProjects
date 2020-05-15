588538b1c49f498c8ea5192f99c6d93444ee84ec
Luis Quintanilla <lqdev@outlook.com>
2020-05-15 10:14:28 -0400

    Updating last streeams changes

---
+++ STAMINA/App/Program.fs
@@ -0,0 +1,19 @@
+open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    // 1. Load binary
+
+    let binaryFile = File.ReadAllBytes("mybinary.txt")
+
+    0
+
+    
+    // 2. Convert to image
+    
+    
+    // 3. Use transfer learning to train a model to classify images
+    
+    
+    // 4. Evaluate model
