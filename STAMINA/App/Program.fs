33f63b5e16f015ae7a37b009e9dbaf23f9502fe9
Luis Quintanilla <lqdev@outlook.com>
2020-05-15 11:41:27 -0400

    Converted file to bytes

--- STAMINA/App/Program.fs
+++ STAMINA/App/Program.fs
@@ -1,13 +1,46 @@
 open System
 open System.IO
+open System.Drawing
+open Microsoft.ML
+open Microsoft.ML.Data
+open Microsoft.ML.Transforms
+
+
+[<CLIMutable>]
+type Img = {
+    [<VectorType(16384)>]
+    Features: byte array
+}
 
 [<EntryPoint>]
 let main argv =
     // 1. Load binary
 
-    let binaryFile = File.ReadAllBytes("mybinary.txt")
+    // Byte[43] 
+    let binaryFileBytes = File.ReadAllBytes("mybinary.txt")
+    
+    let imgWidth = 128
+    let imgHeight = 128
+    let dims = imgWidth * imgHeight
+    let pad = dims - binaryFileBytes.Length
+    let imgBytes = 
+        Array.zeroCreate<byte> pad
+        |> Array.append  binaryFileBytes
+    
+
+
+    let ctx = MLContext()
+    let idv = ctx.Data.LoadFromEnumerable([|{Features=imgBytes}|])
+
+    let pipeline = 
+        EstimatorChain()
+            .Append(ctx.Transforms.ConvertToImage(128,128,"Image","Features"))
+
+    let transformed = pipeline.Fit(idv).Transform(idv)
 
     0
+
+    
 
     
     // 2. Convert to image
--- STAMINA/App/scratch.fsx
+++ STAMINA/App/scratch.fsx
@@ -1,1 +1,30 @@
+open System.IO
 
+
+let filePath = Path.Combine(__SOURCE_DIRECTORY__,"mybinary.txt")
+printfn "%s" filePath
+let binaryFileBytes = File.ReadAllBytes(filePath)
+
+printfn "%A" binaryFile
+
+let imgWidth = 128
+let imgHeight = 128
+let dims = imgWidth * imgHeight
+let pad = dims - binaryFileBytes.Length
+let imgBytes = 
+    Array.zeroCreate<byte> pad
+    |> Array.append  binaryFileBytes
+
+imgBytes.Length
+
+
+#I "C:/Users/lqdev/.nuget/packages"
+#r "system.drawing.common/4.7.0/lib/net461/System.Drawing.Common.dll"
+
+// #r "nuget:System.Drawing.Common"
+
+open System.Drawing
+
+use ms = new MemoryStream(imgBytes)
+let img = Image.FromStream(ms)
+img.Save("img.jpeg")
