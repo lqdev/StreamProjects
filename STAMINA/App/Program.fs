585c54299fc2f12871b7d77b61baee6aff98b1aa
Luis Quintanilla <lqdev@outlook.com>
2020-05-20 11:12:03 -0400

    Converted bytes to image

--- STAMINA/App/Program.fs
+++ STAMINA/App/Program.fs
@@ -1,42 +1,50 @@
 open System
 open System.IO
 open System.Drawing
-open Microsoft.ML
-open Microsoft.ML.Data
-open Microsoft.ML.Transforms
 
+let getFilesBytes (filePath:string) = 
+    File.ReadAllBytes filePath |> Array.map float32
 
-[<CLIMutable>]
-type Img = {
-    [<VectorType(16384)>]
-    Features: byte array
-}
+let padArrayWithZeros (n:int) (firstArray:float32 array) = 
+    Array.zeroCreate<float32> n |> Array.append firstArray
+
+let setColors (vals:float32 array) position a r g b = 
+    let a' = vals.[position + a]
+    let r' = vals.[position + r]
+    let g' = vals.[position + g]
+    let b' = vals.[position + b]
+    a',r',g',b'
 
 [<EntryPoint>]
 let main argv =
     // 1. Load binary
 
     // Byte[43] 
-    let binaryFileBytes = File.ReadAllBytes("mybinary.txt")
+    // let binaryFileBytes = File.ReadAllBytes("mybinary.txt")
     
-    let imgWidth = 128
-    let imgHeight = 128
-    let dims = imgWidth * imgHeight
+    let binaryFileBytes = getFilesBytes "stamina-scalable-deep-learning-whitepaper.pdf"
+
+    let imgWidth = 2048
+    let imgHeight = 2048
+    let dims = imgWidth * imgHeight * 3
     let pad = dims - binaryFileBytes.Length
-    let imgBytes = 
-        Array.zeroCreate<byte> pad
-        |> Array.append  binaryFileBytes
+    let imagePixels = padArrayWithZeros pad binaryFileBytes
     
+    let a,r,g,b = 255,0,0,0
+
+    let dst = new Bitmap(imgHeight,imgWidth)
+
+    for y in 0..2047 do
+        for x in 0..2047 do
+            let position = y * 2048 + x
+            let a',r',g',b' = setColors imagePixels position a r g b
+            let pixel = Color.FromArgb((int)a',(int)r',(int)g',(int)b')
+
+            dst.SetPixel(x,y,pixel)
+
+    dst.Save("fileImage.jpg",Imaging.ImageFormat.Jpeg)
 
 
-    let ctx = MLContext()
-    let idv = ctx.Data.LoadFromEnumerable([|{Features=imgBytes}|])
-
-    let pipeline = 
-        EstimatorChain()
-            .Append(ctx.Transforms.ConvertToImage(128,128,"Image","Features"))
-
-    let transformed = pipeline.Fit(idv).Transform(idv)
 
     0
 
