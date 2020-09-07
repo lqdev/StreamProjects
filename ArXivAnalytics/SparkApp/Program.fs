3ad773d547a3cc23944ce3c17f33432ca0cb1e04
Ubuntu <azuser@fsharp-analytics-vm.g4bmeogvn2fuvmkwhi5jdgkule.bx.internal.cloudapp.net>
2020-09-07 15:05:18 +0000

    Spark app working

---
+++ ArXivAnalytics/SparkApp/Program.fs
@@ -0,0 +1,33 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Microsoft.Spark
+open Microsoft.Spark.Sql
+
+let DATA_DIR = "/datadrive/Data/Kaggle/arxiv-metadata-oai-snapshot-2020-08-14.json"
+
+[<EntryPoint>]
+let main argv =
+    
+    let sparkSession = 
+        SparkSession
+            .Builder()
+            .AppName("arxiv-analytics")
+            .GetOrCreate()
+
+    let arxivData = 
+        sparkSession
+            .Read()
+            .Json([|DATA_DIR|])
+    
+    let categories = 
+        arxivData
+            .Select(Functions.Col("categories").Alias("categories"))
+            .GroupBy("categories")
+            .Count()
+            .OrderBy(Functions.Col("count").Desc())
+
+    categories.Show(10)
+
+    sparkSession.Stop()
+    0 // return an integer exit code
---
+++ ArXivAnalytics/SparkApp/run.sh
@@ -0,0 +1,1 @@
+spark-submit --class org.apache.spark.deploy.dotnet.DotnetRunner --master local ./bin/Debug/netcoreapp3.1/microsoft-spark-2.4.x-0.12.1.jar dotnet ./bin/Debug/netcoreapp3.1/SparkApp.dll
